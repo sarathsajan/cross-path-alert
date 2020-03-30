1fbae025affa1af6e20b61ec399cccdad544e989
NEO <bestinantunps@gmail.com>
2020-03-30 14:41:19 +0530

    corona time

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
-import time
+from flask import Flask, render_template,redirect, url_for, request
+import random
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'cross-path-alert'
 
-@app.route('/')
-def home():
-    return render_template("home.html")
+@app.route("/")
+
+def main():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
