32d990d0f80085652c6eaa579563d8e64ff57685
Sarath Sajan <sarathsajan1998@gmail.com>
2020-03-30 12:43:28 +0530

    added the flask boiler-plate code and reqd directories

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-from flask import flask
+from flask import Flask
 import time
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'cross-path-alert'
+
+@app.route('/')
+def home():
+    return ("<h1>Stay put, Stay Safe</h1>")
+
+if __name__ == "__main__":
+    app.run()
