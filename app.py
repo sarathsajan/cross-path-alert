993971a3b20e9bf8909f538f9589acd41e83c64d
NEO <bestinantunps@gmail.com>
2020-03-30 15:51:13 +0530

    updated with about and login

--- app.py
+++ app.py
@@ -4,9 +4,28 @@ import random
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+
+def main():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])           #login part can be done better
+
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('one'))
+    return render_template('login.html', error=error)
+
+
+@app.route("/about")
+def about():
+   return render_template('about.html')
+
+
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
