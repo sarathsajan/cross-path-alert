87e7cbdcf1784b4af7bebf1e2cfb13b1887422a6
Sarath Sajan <sarathsajan1998@gmail.com>
2020-03-30 18:34:59 +0530

    did minor updates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template,redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request
 import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "cross-path-alert"
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
 
