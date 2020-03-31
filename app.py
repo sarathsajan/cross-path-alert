3a1023ebd3cd175905bc00409736415bf5b1adfd
Sarath Sajan <sarathsajan1998@gmail.com>
2020-03-31 23:24:34 +0530

    Added mock databse as data.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import random
+import data.py
 from flask import Flask, render_template, redirect, url_for, request
 # A more formal packaging needed... with __init__.py as the development grows.
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "cross-path-alert"
+
+database = database()
 
 @app.route('/')
 def home():
@@ -11,16 +14,11 @@ def home():
 
 @app.route('/login/')
 def login():
-    return render_template('login.html')
+    return ('login.html', data=database)
 
 @app.route("/about")
 def about():
     return render_template('about.html')
-
-
-@app.route("/signup")
-def signup():
-    return render_template('signup.html')    
 
 if __name__ == "__main__":
     app.run(debug=True)    # Remember to remove debug=True in production
