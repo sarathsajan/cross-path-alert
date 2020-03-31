994cb4ae3b51db345a718b7cd7c91db7d02b9928
NEO <bestinantunps@gmail.com>
2020-03-31 23:50:22 +0530

    minor updates in template

--- app.py
+++ app.py
@@ -12,15 +12,12 @@ def home():
 @app.route('/login/')
 def login():
     return render_template('login.html')
+       
 
 @app.route("/about")
 def about():
     return render_template('about.html')
-
-
-@app.route("/signup")
-def signup():
-    return render_template('signup.html')    
+    
 
 if __name__ == "__main__":
     app.run(debug=True)    # Remember to remove debug=True in production
