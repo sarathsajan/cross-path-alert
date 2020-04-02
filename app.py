264b619a1a38c928816ca31012b1c05499628dd2
NEO <bestinantunps@gmail.com>
2020-04-02 15:19:57 +0530

    ADDED DIST ALGORITHM

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import random
+from math import radians, cos, sin, asin, sqrt  
 from data import database
 from flask import Flask, render_template, redirect, url_for, request
 # A more formal packaging needed... with __init__.py as the development grows.
@@ -7,22 +8,29 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "cross-path-alert"
 
 database = database()
+traveldatabase = []
 
-@app.route('/')
-def home():
-	return render_template('home.html')
+def distance(lat1, lat2, lon1, lon2): 
+      
+    lon1 = radians(lon1)       #converting radians
+    lon2 = radians(lon2) 
+    lat1 = radians(lat1) 
+    lat2 = radians(lat2) 
+        
+    dlon = lon2 - lon1         # Haversine formula 
+    dlat = lat2 - lat1 
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * asin(sqrt(a))  
+      
+    r = 6371                     #Radius of earth
+        
+    return(c * r)                #final output
 
-@app.route('/login/')
-def login():
-    return render_template('login.html')
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
-  
-if __name__ == "__main__":
-    app.run(debug=True)    # Remember to remove debug=True in production
+
+
+print(distance(10.5276,10.5221,76.2144,76.2237))
+
+#for x in database:
+#	print(x['travel_history'])
---
+++ backup.py
@@ -0,0 +1,23 @@
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "cross-path-alert"
+
+database = database()
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+  
+if __name__ == "__main__":
+    app.run(debug=True)    # Remember to remove debug=True in production
