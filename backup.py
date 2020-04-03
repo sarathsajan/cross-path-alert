58c87207c10316af88e6e19b8a69066fb7cbdb10
NEO <bestinantunps@gmail.com>
2020-04-03 20:18:05 +0530

    two basic maps

--- backup.py
+++ backup.py
@@ -1,3 +1,12 @@
+import random
+from datetime import datetime
+from math import radians, cos, sin, asin, sqrt  
+from data import database
+from user_data import userdetails
+from flask import Flask, render_template, redirect, url_for, request
+# A more formal packaging needed... with __init__.py as the development grows.
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "cross-path-alert"   #This is nothin but the flask backup
 
@@ -18,6 +27,10 @@ def signup():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/maps")
+def maps():
+    return render_template('maps.html')    
   
 if __name__ == "__main__":
     app.run(debug=True)    # Remember to remove debug=True in production
