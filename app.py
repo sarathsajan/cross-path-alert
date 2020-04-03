2bae9d0cf92503ab08fcdb8eeaf491a1174e2068
Sarath Sajan <sarathsajan1998@gmail.com>
2020-04-03 23:28:02 +0530

    added database connection, now you can sign up and login

--- app.py
+++ app.py
@@ -24,9 +24,7 @@ mysql = MySQL(app)
 def home():
 	return render_template('home.html')
 
-@app.route('/login/')
-def login():
-    return render_template('login.html')
+
 
 ###################################### primary register form ########################
 class RegisterForm(Form):                                                           #
@@ -51,7 +49,7 @@ def signup():
         cur = mysql.connection.cursor()
 
         # Execute mysql commands
-        cur.execute("INSERT INTO users(name, email, password) VALUES(%s,%s, %s)", (name, email, password))
+        cur.execute("INSERT INTO users(name, email, password) VALUES(%s, %s, %s)", (name, email, password))
 
         # Commit to the database
         mysql.connection.commit()
@@ -63,6 +61,45 @@ def signup():
 
         return redirect(url_for('login'))
     return render_template('signup.html', form=form)
+
+
+
+###################################### primary login form ###########################
+class LoginForm(Form):                                                              #
+    email = StringField('Registered Email', [validators.Length(min=6, max=50)])     #
+    password = PasswordField('Password', [validators.DataRequired()])               #
+#####################################################################################
+
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        candidate_email = form.email.data
+        candidate_password = form.password.data
+
+        # Create a connection and initialise a cursor to interface flask, python and mysql
+        cur = mysql.connection.cursor()
+
+        # Execute mysql commands
+        # Fetch username from the database
+        result = cur.execute("SELECT * FROM users WHERE email = %s", [candidate_email])
+
+        if result > 0:
+            data = cur.fetchone()
+            password = data['password']
+            if sha256_crypt.verify(candidate_password, password):
+                flash("You are now logged in", 'success')
+                return redirect(url_for('home'))
+            else:
+                error = "Invalid Password"
+                return render_template("login.html", form=form, error=error)
+        else:
+            error = "No user with that E-mail ID"
+            return render_template("login.html", form=form, error=error)
+
+    return render_template('login.html', form=form)
+
+
 
 @app.route("/about/")
 def about():
