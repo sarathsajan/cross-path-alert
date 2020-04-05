3a403d51b506b34ee95cc89e0a4f7c5a78b7704c
NEO <bestinantunps@gmail.com>
2020-04-06 01:44:27 +0530

    naice

---
+++ app.py
@@ -0,0 +1,19 @@
+
+from data import userdetails
+from datetime import datetime
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+# A more formal packaging needed... with __init__.py as the development grows.
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "cross-path-alert"
+
+data = userdetails()
+
+
+@app.route('/test')
+def test():
+   return render_template('test.html', output_data = data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)    # Remember to remove debug=True in production 
---
+++ data.py
@@ -0,0 +1,39 @@
+traveldata = [
+                {
+                    'date' : '2020-03-01',
+                    'time' : '7-15',
+                    'latitude' : '10.5206', 
+                    'longtitude': '76.2140',
+                    'flag' : '0'
+                                     
+                },
+                {
+
+                    'date' : '2020-03-01',
+                    'time' : '15-00',
+                    'latitude' : '10.5206', 
+                    'longtitude': '76.2140',
+                    'flag' : '1'
+                                        
+                                    
+                },
+                {
+
+                    'date' : '2019-03-01',
+                    'time' : '01-15',
+                    'latitude' : '10.5206', 
+                    'longtitude': '76.2140',
+                    'flag' : '2'
+                },
+                {
+                    'date' : '2020-03-01',
+                    'time' : '15-00',
+                    'latitude' : '10.5206', 
+                    'longtitude': '76.2140',
+                    'flag' : '3'
+                }
+                                    
+                
+            ]
+def userdetails():
+    return traveldata
