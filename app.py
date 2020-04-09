0f4f0b1a3f4752a19ffed9a170fff2e366462708
NEO <bestinantunps@gmail.com>
2020-04-10 02:58:04 +0530

    m

--- app.py
+++ app.py
@@ -1,28 +1,8 @@
-import random
-from data import database
-from flask import Flask, render_template, redirect, url_for, request
-# A more formal packaging needed... with __init__.py as the development grows.
+import requests,json
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "cross-path-alert"
-
-database = database()
-
-@app.route('/')
-def home():
-	return render_template('home.html')
-
-@app.route('/login/')
-def login():
-    return render_template('login.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-  
-if __name__ == "__main__":
-    app.run(debug=True)    # Remember to remove debug=True in production
+url="https://covid19-kerala-api.herokuapp.com/api/location?date=latest"
+data=requests.get(url)
+data=data.json()
+json_string = json.dumps(data)
+y=json.loads(json_string)
+print()
